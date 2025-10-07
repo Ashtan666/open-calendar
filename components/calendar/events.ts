dcbbaa90343139351be3d7cfb45f5c20467cc0cd
ashtan666 <s.ashtan666@gmail.com>
2025-10-07 09:25:19 +0900

    feat(event): load events from localStorage and render them in calendar (#5)

--- components/calendar/events.ts
+++ components/calendar/events.ts
@@ -1,3 +1,5 @@
+import { CalendarEvent } from "@/utils/localStrage";
+
 // create sample event
 const today = new Date();
 
@@ -7,25 +9,54 @@ const date = {
   day: today.getDate(),
 };
 
-const events = [
+// const events= [
+//   {
+//     title: "Event1",
+//     start: new Date(),
+//     color: "green", // override!
+//   },
+//   {
+//     title: "Event2",
+//     start: new Date(),
+//     color: "green", // override!
+//   },
+//   {
+//     title: "Event3",
+//     start: new Date(date.year, date.month - 1, date.day + 3),
+//   },
+//   {
+//     title: "Weekly Event",
+//     start: new Date(date.year, date.month - 1, date.day),
+//     end: new Date(date.year, date.month - 1, date.day + 6),
+//   },
+// ];
+
+/*-------------------------*/
+// id: string;
+// title: string;
+// date: string;
+// description?: string;
+/*-------------------------*/
+const events: CalendarEvent[] = [
   {
-    title: "Event1",
-    start: new Date(),
-    color: "green", // override!
+    id: crypto.randomUUID(),
+    title: "Sample1",
+    // date: new Date().toString(),
+    // date: "2025-10-07",
+    // sv-SE locale returns a date stirng in the format YYYY-MM-DD
+    date: new Date().toLocaleDateString("sv-SE"),
+    description: "Description of Sample1",
   },
   {
-    title: "Event2",
-    start: new Date(),
-    color: "green", // override!
-  },
-  {
-    title: "Event3",
-    start: new Date(date.year, date.month - 1, date.day + 3),
-  },
-  {
-    title: "Weekly Event",
-    start: new Date(date.year, date.month - 1, date.day),
-    end: new Date(date.year, date.month - 1, date.day + 6),
+    id: crypto.randomUUID(),
+    title: "Sample2",
+    // date: new Date().toString(),
+    // date: "2025-10-07",
+    // sv-SE locale returns a date stirng in the format YYYY-MM-DD
+    date: new Date(date.year, date.month - 1, date.day + 3).toLocaleDateString(
+      "sv-SE"
+    ),
+    description: "Description of Sample1",
   },
 ];
 
